395bbe8fbd59856109dfd6b2f417da4f8a8c1b60
jack <hongyu973@163.com>
2020-06-22 22:55:49 +0800

    初始化项目信息

---
+++ src/main/java/com/example/citex_fusion/CitexFusionApplication.java
@@ -0,0 +1,13 @@
+package com.example.citex_fusion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitexFusionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitexFusionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/citex_fusion/CitexFusionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.citex_fusion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitexFusionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
